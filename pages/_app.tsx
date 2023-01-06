03888c02f36602801a2d8606c7a28bb021b650a5
nicolehu26 <nicolehu2626@gmail.com>
2023-01-05 22:05:26 -0700

    trying ganalytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,13 +4,20 @@ import Script from 'next/script'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <>
-  <Script>{`!function(){var analytics=window.analytics=window.analytics||[];if(!analytics.initialize)if(analytics.invoked)window.console&&console.error&&console.error("Segment snippet included twice.");else{analytics.invoked=!0;analytics.methods=["trackSubmit","trackClick","trackLink","trackForm","pageview","identify","reset","group","track","ready","alias","debug","page","once","off","on","addSourceMiddleware","addIntegrationMiddleware","setAnonymousId","addDestinationMiddleware"];analytics.factory=function(e){return function(){var t=Array.prototype.slice.call(arguments);t.unshift(e);analytics.push(t);return analytics}};for(var e=0;e<analytics.methods.length;e++){var key=analytics.methods[e];analytics[key]=analytics.factory(key)}analytics.load=function(key,e){var t=document.createElement("script");t.type="text/javascript";t.async=!0;t.src="https://cdn.segment.com/analytics.js/v1/" + key + "/analytics.min.js";var n=document.getElementsByTagName("script")[0];n.parentNode.insertBefore(t,n);analytics._loadOptions=e};analytics._writeKey="qbHTCIxs3mnfbbsdrhAUlDPIj36oW0rX";;analytics.SNIPPET_VERSION="4.15.3";
-  analytics.load("qbHTCIxs3mnfbbsdrhAUlDPIj36oW0rX");
-  analytics.page();
-  }}();`}</Script>
+  <Script async src="https://www.googletagmanager.com/gtag/js?id=G-T4GDMGLEYV"></Script>
+  <Script>{`
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'G-T4GDMGLEYV');;`}
+  </Script>
+
+    
   <Component {...pageProps} />
   </>
 
 }
 
 export default MyApp
+
